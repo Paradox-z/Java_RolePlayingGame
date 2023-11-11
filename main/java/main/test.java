f2b09216c3edc18ae5dfa9e6f63712b787ae94f3
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2023-11-11 11:43:06 +0800

    Update test.java

--- main/java/main/test.java
+++ main/java/main/test.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 public class test {
 //    public static void main(String[] args) {
-//        Select s = new Select();  // Class Select
+//        Select s = new Select();  // Class, Function, choose the appropriate to utilise.
 //
 //        String sql = "select name,hp,maxhp,attack,defend,gold from hero";  // select action
-//        List<Hero> list = s.getForList(Hero.class,sql);  // get all data from the whole table
+//        List<Hero> list = s.getForList(Hero.class,sql);  // retrive all data elements from the whole table.
 //        //System.out.println("\tname\tprice\tauthor\tnum");
 //        list.forEach(System.out::println);
 //
 //        String sql1 = "select name,hp,attack,defend,gold from monster";  // select action
-//        List<Monster> list1 = s.getForList(Monster.class,sql1);  // get all data from the whole table
+//        List<Monster> list1 = s.getForList(Monster.class,sql1);  // retrieve all data elements from the whole table.
 //        list1.forEach(System.out::println);
 //
 //    }
